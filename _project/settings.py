76a61c6f9c1de902739e0ff084990fafbcd97285
Basil Dubyk <samitnuk.ua@gmail.com>
2017-02-02 22:51:03 +0200

    Add cart app

--- _project/settings.py
+++ _project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.shop',
+    'apps.cart',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+
+CART_SESSION_ID = 'cart'
