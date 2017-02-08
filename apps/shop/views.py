1da02db9636d722a82044a20ce965ad04b126135
Basil Dubyk <samitnuk.ua@gmail.com>
2017-02-08 21:31:59 +0200

    Update product_detail view

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
+from ..cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -21,4 +22,8 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/detail.html', {'product': product})
+    context = {
+        'product': product,
+        'cart_product_form': CartAddProductForm()
+    }
+    return render(request, 'shop/detail.html', context)
