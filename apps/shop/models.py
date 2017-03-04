ff1c3f4d5f8ac53c6746e3976c89c20e4d5fa179
Basil Dubyk <samitnuk.ua@gmail.com>
2017-03-04 23:09:07 +0200

    Update models and add Manufacturer Model

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    slug = models.SlugField(max_length=200, db_index=True,
+                            unique=True, allow_unicode=True)
     parent_category = models.ForeignKey('self', null=True, blank=True)
 
     class Meta:
@@ -20,6 +21,10 @@ class Category(models.Model):
             'shop:product_list_by_category',
             kwargs={'category_slug': self.slug})
 
+    @property
+    def products(self):
+        return self.category_products.all()
+
     def subcategories(self):
         return Category.objects.filter(parent_category__id=self.id)
 
@@ -27,7 +32,7 @@ class Category(models.Model):
         return True if self.parent_category else False
 
     @property
-    def full_name(self):
+    def full_name(self):  # to show in admin panel
         if not self.has_parent_category():
             return self.name
         cat = self
@@ -38,9 +43,35 @@ class Category(models.Model):
         return full_name
 
 
+class Manufacturer(models.Model):
+    name = models.CharField(
+        max_length=200, db_index=True, verbose_name="Виробник")
+    slug = models.SlugField(
+        max_length=200, db_index=True, allow_unicode=True)
+
+    class Meta:
+        verbose_name = "Виробник"
+        verbose_name_plural = "Виробники"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_detail',
+            kwargs={'id': self.id, 'slug': self.slug})
+
+    @property
+    def products(self):
+        return self.manufacturer_products.all()
+
+
 class Product(models.Model):
     category = models.ForeignKey(
-        Category, related_name='products', verbose_name="Категорія")
+        Category, related_name='category_products', verbose_name="Категорія")
+    manufacturer = models.ForeignKey(
+        Manufacturer, related_name='manufacturer_products',
+        verbose_name="Виробник", blank=True, null=True)
     name = models.CharField(
         max_length=200, db_index=True, verbose_name="Назва")
     slug = models.SlugField(
