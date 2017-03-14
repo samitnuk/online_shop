983b363545962cbd69af5dbaed3eba00bc85ebb5
Basil Dubyk <samitnuk.ua@gmail.com>
2017-03-14 23:10:03 +0200

    Update views

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from ..cart.cart import Cart
 
 
+@login_required
 def order_create(request):
     cart = Cart(request)
     form = OrderCreateForm(request.POST or None)
     if form.is_valid():
         order = form.save()
         for item in cart:
-            OrderItem.objects.create(order=order,
-                                     product=item['product'],
-                                     price=item['price'],
-                                     quantity=item['quantity'])
+            OrderItem.objects.create(
+                order=order,
+                product=item['product'],
+                price=item['price'],
+                quantity=item['quantity'],
+            )
             cart.clear()
             return render(request, 'orders/created.html', {'order': order})
 
