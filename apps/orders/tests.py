bee190a6a4e39e2c74e12db62d2587062e7d33fa
Basil Dubyk <samitnuk.ua@gmail.com>
2017-03-18 09:33:32 +0200

    Update tests for orders app

--- apps/orders/tests.py
+++ apps/orders/tests.py
@@ -1,10 +1,10 @@
 from django_webtest import WebTest
 
-from django.conf import settings
 from django.shortcuts import reverse
 
 from apps.shop.models import Product
 from apps.shop import utils
+from apps.orders.models import Order
 
 
 class OrdersWebTests(WebTest):
@@ -12,12 +12,21 @@ class OrdersWebTests(WebTest):
     fixtures = ['../load_data.json']
 
     def test_order_creation(self):
+        # Login user
+        user = utils.get_regular_user()
+        form = self.app.get(reverse('accounts:login')).form
+        form['username'] = user.username
+        form['password'] = "asdkjfoih1222pkljkh"
+        form.submit().follow()
+
+        # Get products for tests
         products = Product.objects.all()
         product1 = products.order_by('?').first()
         product2 = products.order_by('?').first()
         while product1 == product2:
             product2 = products.order_by('?').first()
-        print(product1, product2)
+
+        # Add 1st product to cart
         form = self.app.get(
             reverse(
                 'shop:product_detail',
@@ -28,6 +37,7 @@ class OrdersWebTests(WebTest):
         form['quantity'] = qty
         form.submit()
 
+        # Add 2nd product to cart
         form = self.app.get(
             reverse(
                 'shop:product_detail',
@@ -37,21 +47,25 @@ class OrdersWebTests(WebTest):
         qty = product2.stock - 1 if product2.stock != 1 else 1
         form['quantity'] = qty
         form.submit()
-        cart = self.app.session[settings.CART_SESSION_ID]
-        print(cart)
-        user = utils.get_staff_member()
-        print(user)
-        form = self.app.get(reverse('orders:create'), user=user)
-        # form['first_name'] = "Test_first_name"
-        # form['last_name'] = "Test_last_name"
-        # form['carrier'] = "Test carrier"
-        # form['city'] = "Testtown"
-        # form['warehouse_num'] = "2"
-        # form['phone_num'] = "(000) 11-22-333"
-        # response = form.submit().follow()
-        #
-        # user = response.context['user']
-        #
-        # order = user.products.first()
-        #
-        # self.assertEqual(order.first_name, "Test_first_name")
+
+        # Create order
+        form = self.app.get(reverse('orders:create')).form
+        form['first_name'] = "Test_first_name"
+        form['last_name'] = "Test_last_name"
+        form['carrier'] = "Test carrier"
+        form['city'] = "Testtown"
+        form['warehouse_num'] = "2"
+        form['phone_num'] = "(000) 11-22-333"
+        response = form.submit()
+
+        user = response.context['user']
+
+        order = Order.objects.filter(user=user).first()
+
+        self.assertEqual(order.first_name, "Test_first_name")
+        self.assertEqual(order.last_name, "Test_last_name")
+        self.assertEqual(order.carrier, "Test carrier")
+        self.assertEqual(order.city, "Testtown")
+        self.assertEqual(order.warehouse_num, 2)
+        self.assertEqual(order.phone_num, "(000) 11-22-333")
+        self.assertFalse(order.paid)
