4acd83f7bb04cd3dd7e725e75514c8ea28b312ea
Basil Dubyk <samitnuk.ua@gmail.com>
2017-03-23 23:09:44 +0200

    Update coupons app

--- apps/coupons/admin.py
+++ apps/coupons/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Coupon
+
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
+    list_filter = ['valid_from', 'valid_to', 'active']
+    search_fields = ['code']
+
+
+admin.site.register(Coupon, CouponAdmin)
--- apps/coupons/views.py
+++ apps/coupons/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .forms import CouponApplyForm
+from .models import Coupon
+
+
+@require_POST
+def coupon_apply(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data['code']
+        try:
+            coupon = Coupon.objects.get(code__iexact=code,
+                                        valid_from__lte=now,
+                                        valid_to__gte=now,
+                                        active=True)
+            request.session['coupon_id'] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+    return redirect('cart:detail')
+
